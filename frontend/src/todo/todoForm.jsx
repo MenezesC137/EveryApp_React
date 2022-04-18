60a884418ca6da90ea90a58da77aa4dff751b84d
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-04-18 12:19:42 -0300

    Evento Add

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 
 export default props => (
     <div role='form' className='todoForm'>
@@ -12,8 +13,10 @@ export default props => (
         </Grid>
        
         <Grid cols='12 3 2'>
-            <button className='btn btn-primary'>
-                <i className='fa fa-plus'></i>
+            <button className='primary'>
+                <IconButton style='primary' icon='plus'
+                    onClick={props.handleAdd}>
+                </IconButton>
             </button>
         </Grid>        
     </div>
